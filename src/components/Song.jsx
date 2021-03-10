5aba99d89cb5b4b421e86390e0df9de62f16072e
edavid72 <david.cervellon72@gmail.com>
2021-03-10 15:23:38 -0600

    gh-pages update

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,9 +2,8 @@ import React, {Fragment} from 'react';
 
 const Song = ({savelyric}) => {
 
-    if (savelyric.length === 0) return null; {
-        
-    }
+    if (savelyric.length === 0) return null;
+    
     return ( 
         <Fragment>
             <h2>Lyric Song</h2>
